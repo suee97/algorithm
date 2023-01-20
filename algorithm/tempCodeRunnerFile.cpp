63737ff9e854e602ad27ad2b009183101cfb7227
suee97 <atn1su@gmail.com>
2023-01-20 23:27:46 +0900

    6593

--- algorithm/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //         cin >> board[i][j];
-    //         if (board[i][j] > maxHeight) {
-    //             maxHeight = board[i][j];
-    //         }
-    //     }
-    // }
