96514932671a258c8c83a27aced80b6522d0c953
suee97 <atn1su@gmail.com>
2023-03-02 00:21:57 +0900

    11727

--- algorithm/16401.cpp
+++ algorithm/16401.cpp
@@ -1,6 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    
-}
+// 못 풂
+// https://se-jung-h.tistory.com/entry/BOJC-%EB%B0%B1%EC%A4%80-16401-%EA%B3%BC%EC%9E%90-%EB%82%98%EB%88%A0%EC%A3%BC%EA%B8%B0
